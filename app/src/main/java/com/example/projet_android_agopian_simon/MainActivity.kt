1edd420778a68a7ac73b3efd3e2481cf805be897
Paul Simon <paul.simon@signature-code.com>
2020-11-27 19:15:20 +0100

    photo and okHttp

--- app/src/main/java/com/example/projet_android_agopian_simon/MainActivity.kt
+++ app/src/main/java/com/example/projet_android_agopian_simon/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.projet_android_agopian_simon
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projet_android_agopian_simon.fragment.HomeFragment
 import com.example.projet_android_agopian_simon.fragment.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    var REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +27,11 @@ class MainActivity : AppCompatActivity() {
         val fragment_profile  = ProfileFragment()
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
+        val buttonPhoto = findViewById<Button>(R.id.button_Photo)
+        buttonPhoto.setOnClickListener(View.OnClickListener { c: View? -> onClickPhoto() })
+
+
+
 
 
         BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -41,6 +58,37 @@ class MainActivity : AppCompatActivity() {
                 }
                 else -> false
             }
+        }
+    }
+
+    fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent,  REQUEST_IMAGE_CAPTURE)
+        }catch (e: ActivityNotFoundException) {
+            Log.e("error", "can't take photo")
+        }
+
+    }
+
+
+    fun onClickPhoto() {
+        dispatchTakePictureIntent()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val extras = data?.extras
+            val imageBitmap = extras!!["data"] as Bitmap?
+
+            //ImageView imageView = findViewById(R.id.imageView2);
+            CapturePhotoUtils.insertImage(this.contentResolver, imageBitmap, "title", "uberIMG")
+            val text = "Image saved!"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
         }
     }
 }
